3b4329d7110723220e97d31d694b8d207f14c658
pyan83 <puyanwei@gmail.com>
2017-08-01 16:29:29 +0100

    Added test to check for init and that it's not nil.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-  it "Docking station should not be nil" do
+  it "docking station should not be nil" do
 
-  expect(DockingStation.new).not_to eq(nil)
+  docking_station = DockingStation.new #Defined var w/type Dockingstation
+  expect(docking_station).not_to eq(nil)#Check initialization and that its not nil
   end
 end
